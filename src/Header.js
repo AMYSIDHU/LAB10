59ef80308e399db5d8e70c4afde3850412e3cac0
AMYSIDHU <amy.sidhu@edu.sait.ca>
2022-04-22 21:28:22 -0600

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ function Header() {
         <div className="container py-5">
             <div className="row">
                 <div className="col-xs-3 col-lg-2 text-center">
-                    <img src={ProfilePic} alt="Anime Profile Pic" className="border border-5 rounded-circle img-fluid" style={{width: "150px", height: "150px" }}/>
+                    <img src={ProfilePic} alt="Profile Pic" className="border border-5 rounded-circle img-fluid" style={{width: "150px", height: "150px" }}/>
                 </div>
                 <div className="col text-center text-lg-start">
                     <h1>Amy Sidhu</h1>
